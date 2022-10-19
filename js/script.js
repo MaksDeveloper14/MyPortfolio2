6a064fff9d6bc0bc183de9fa1abe5b5320c1f86d
Maks14 <Maksim-volkov1994@yandex.ru>
2022-10-20 02:21:21 +0300

    Добавил выбор картинки пользователем и вставка в HTML, но корявым способом

--- js/script.js
+++ js/script.js
@@ -23,8 +23,37 @@ setInterval(() => {
 
 
 
+// МОИ РАБОТЫ
 
+// ~~~~~~~~~~~~~~~ script 2: FILE API ~~~~~~~~~~~~~~~~~
 
-// ~~~~~~~~~~~~~~~ script 2:  ~~~~~~~~~~~~~~~~~
+// Проверяем поддержку File API 
+if (window.File && window.FileReader && window.FileList && window.Blob) {
 
+} else {
+  alert('File API не поддерживается данным браузером');
+}
 
+const inputFile = document.querySelector('.input-file');
+const myWorksRow = document.querySelector('.my-works__row');
+
+inputFile.onchange = function() {
+	let file = inputFile.files[0];
+	let reader = new FileReader();
+	let src = URL.createObjectURL(file)
+	let img = new Image();
+	img.src = src;
+
+	// сниппет карточка
+	let card = `
+		<div class="col my-works__col">
+			<div class="card" style="width: 18rem;">
+			  ${img.outerHTML}
+			  <div class="card-body">
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			  </div>
+			</div>
+    </div>
+	`
+	myWorksRow.insertAdjacentHTML('beforeend', card);
+}
